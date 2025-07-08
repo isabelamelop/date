7651e8a9d66e933fc20a7463ee9fd2e2fa64e02c
isa pereira <128876888+isabelamelop@users.noreply.github.com>
2025-07-08 13:13:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ window.onload = () => {
     const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
         clearTimeout(c);
-    }, 1000);
+        
+        // Exibir a caixa de mensagem 2 segundos após a animação da flor
+        setTimeout(() => {
+            document.getElementById("conteudo").style.display = "block"; // Exibe a mensagem
+        }, 2000); // 2 segundos após a animação da flor
+    }, 1000); // Tempo para remover a classe not-loaded
 };
